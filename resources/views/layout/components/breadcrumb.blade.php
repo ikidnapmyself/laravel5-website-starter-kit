25f3c7539ea48f2d5c0ca9e2dc026a9cb080f852
frnxstd <burak@myself.com>
2018-07-13 01:55:53 +0300

    breadcrumb component

--- resources/views/layout/components/breadcrumb.blade.php
+++ resources/views/layout/components/breadcrumb.blade.php
@@ -1,15 +1,6 @@
-<div class="row">
-    <div class="col-sm-6">
-
-        <h4>@yield('title')</h4>
-
-    </div><!-- col -->
-    <div class="col-sm-6">
-
-        <ol class="breadcrumb">
-            <li><a href="{{ url('/') }}">@lang('pages.menu.Anasayfa')</a></li>
-            <li class="active">@yield('title')</li>
-        </ol>
-
-    </div><!-- col -->
-</div><!-- row -->
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ url('/') }}">@lang('pages.menu.Home')</a></li>
+        <li class="breadcrumb-item active" aria-current="page">@yield('title')</li>
+    </ol>
+</nav>
