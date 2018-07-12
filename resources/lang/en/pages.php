9f0da25a655cecaf6a255cd6a940c097477ddb64
frnxstd <burak@myself.com>
2018-07-13 01:47:10 +0300

    languages

--- resources/lang/en/pages.php
+++ resources/lang/en/pages.php
@@ -11,12 +11,16 @@ return [
     'menu' => [
         'Home'    => 'HOME',
         'About '  => 'ABOUT',
+        'Stacks'  => 'STACKS',
+        'Gallery' => 'GALLERY',
         'Contact' => 'Contact',
     ],
 
     'title' => [
         'Home'    => 'Home',
         'About'   => 'About',
+        'Stacks'  => 'Stacks',
+        'Gallery' => 'Gallery',
         'Contact' => 'Contact',
     ],
 ];
--- resources/lang/tr/pages.php
+++ resources/lang/tr/pages.php
@@ -11,12 +11,14 @@ return [
     'menu' => [
         'Home'    => 'ANASAYFA',
         'About'   => 'HAKKIMIZDA',
+        'Stacks'  => 'YIĞINLAR',
         'Contact' => 'İLETİŞİM',
     ],
 
     'title' => [
         'Home'    => 'Anasayfa',
         'About'   => 'Hakkımızda',
+        'Stacks ' => 'Stacks',
         'Contact' => 'İletişim',
     ],
 ];
